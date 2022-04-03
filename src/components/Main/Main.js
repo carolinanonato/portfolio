f7e1630fb622aad3dccdff7c5fdac9f0b2c876d9
Carolina Nonato <csnonato@gmail.com>
2022-04-03 11:14:47 -0400

    button padding

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,7 +8,7 @@ export default function Main() {
         <div className='main'>
             <h1 className='main-title'>Carolina Nonato</h1>
             <p className='sub-heading'>Web Design & Development</p>
-            <AnchorLink href='#contact'><button>Contact</button></AnchorLink>
+            <AnchorLink href='#contact'><button className='first-btn'>Contact</button></AnchorLink>
 
         </div>
     )
