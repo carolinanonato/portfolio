25610b5739ad3a849aa17ebf6166a06c7e2a7999
Carolina Nonato <csnonato@gmail.com>
2022-04-02 21:59:22 -0400

    projects hardcoded

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,12 +9,58 @@ export default function Projects() {
                 <h1 className='project-title'>Projects</h1>
             </div>
             <div className="project-cards">
-                <h3 className='project-name'>Dog Food Calculator</h3>
-                <img className="project-image" src={dogfood} alt="doog food" />
-                <p className='project-description'>Fresh food is the best thing for dogs! They became more cheerful, their fur shinier and they hardly get sick. In this project I created a simple app to calculate the servings of meat, vegetables and carbohydrates your dog needs daily.
-                </p>
-                <button><a href="https://carolinanonato.github.io/dog-fresh-food/" target="_blank">Live Demo</a></button>
-                <button><a href="https://github.com/carolinanonato/dog-fresh-food" target="_blank">Code</a></button>
+                <div className="card1">
+                    <h3 className='project-name'>Dog Food Calculator</h3>
+                    <img className="project-image" src={dogfood} alt="doog food" />
+                    <p className='project-description'>Fresh food is the best thing for dogs! They became more cheerful, their fur shinier and they hardly get sick. In this project I created a simple app to calculate the servings of meat, vegetables and carbohydrates your dog needs daily.
+                    </p>
+                    <button className='project-btn'><a href="https://carolinanonato.github.io/dog-fresh-food/" target="_blank">Live Demo</a></button>
+                    <button className='project-btn'><a href="https://github.com/carolinanonato/dog-fresh-food" target="_blank">Code</a></button>
+                </div>
+                <div className="card2">
+                    <h3 className='project-name'>Dog Food Calculator</h3>
+                    <img className="project-image" src={dogfood} alt="doog food" />
+                    <p className='project-description'>Fresh food is the best thing for dogs! They became more cheerful, their fur shinier and they hardly get sick. In this project I created a simple app to calculate the servings of meat, vegetables and carbohydrates your dog needs daily.
+                    </p>
+                    <button className='project-btn'><a href="https://carolinanonato.github.io/dog-fresh-food/" target="_blank">Live Demo</a></button>
+                    <button className='project-btn'><a href="https://github.com/carolinanonato/dog-fresh-food" target="_blank">Code</a></button>
+                </div>
+            </div>
+            <div className="project-cards">
+                <div className="card2">
+                    <h3 className='project-name'>Dog Food Calculator</h3>
+                    <img className="project-image" src={dogfood} alt="doog food" />
+                    <p className='project-description'>Fresh food is the best thing for dogs! They became more cheerful, their fur shinier and they hardly get sick. In this project I created a simple app to calculate the servings of meat, vegetables and carbohydrates your dog needs daily.
+                    </p>
+                    <button className='project-btn'><a href="https://carolinanonato.github.io/dog-fresh-food/" target="_blank">Live Demo</a></button>
+                    <button className='project-btn'><a href="https://github.com/carolinanonato/dog-fresh-food" target="_blank">Code</a></button>
+                </div>
+                <div className="card2">
+                    <h3 className='project-name'>Dog Food Calculator</h3>
+                    <img className="project-image" src={dogfood} alt="doog food" />
+                    <p className='project-description'>Fresh food is the best thing for dogs! They became more cheerful, their fur shinier and they hardly get sick. In this project I created a simple app to calculate the servings of meat, vegetables and carbohydrates your dog needs daily.
+                    </p>
+                    <button className='project-btn'><a href="https://carolinanonato.github.io/dog-fresh-food/" target="_blank">Live Demo</a></button>
+                    <button className='project-btn'><a href="https://github.com/carolinanonato/dog-fresh-food" target="_blank">Code</a></button>
+                </div>
+            </div>
+            <div className="project-cards">
+                <div className="card2">
+                    <h3 className='project-name'>Dog Food Calculator</h3>
+                    <img className="project-image" src={dogfood} alt="doog food" />
+                    <p className='project-description'>Fresh food is the best thing for dogs! They became more cheerful, their fur shinier and they hardly get sick. In this project I created a simple app to calculate the servings of meat, vegetables and carbohydrates your dog needs daily.
+                    </p>
+                    <button className='project-btn'><a href="https://carolinanonato.github.io/dog-fresh-food/" target="_blank">Live Demo</a></button>
+                    <button className='project-btn'><a href="https://github.com/carolinanonato/dog-fresh-food" target="_blank">Code</a></button>
+                </div>
+                <div className="card2">
+                    <h3 className='project-name'>Dog Food Calculator</h3>
+                    <img className="project-image" src={dogfood} alt="doog food" />
+                    <p className='project-description'>Fresh food is the best thing for dogs! They became more cheerful, their fur shinier and they hardly get sick. In this project I created a simple app to calculate the servings of meat, vegetables and carbohydrates your dog needs daily.
+                    </p>
+                    <button className='project-btn'><a href="https://carolinanonato.github.io/dog-fresh-food/" target="_blank">Live Demo</a></button>
+                    <button className='project-btn'><a href="https://github.com/carolinanonato/dog-fresh-food" target="_blank">Code</a></button>
+                </div>
             </div>
         </div>
     )
